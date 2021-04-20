7adc94d261b4ad0105c02ef08bbb26573bf4033c
opfez <opfez@disroot.org>
2021-04-20 11:59:28 +0200

    add - option to cat(1)

--- src/bin/cat.c
+++ src/bin/cat.c
@@ -15,18 +15,24 @@ void cat(int fd) {
 
 int main(int argc, char *argv[]) {
 	if (argc < 2)
-		return 1;  // todo: implement vanilla stdin input
+		return 1;
 	argv++; // we don't care about program name
 
 	for (; *argv != NULL; argv++) {
-		int file = open(*argv, O_RDONLY);
+		int file;
+		if (!strcmp(*argv, "-"))
+			file = STDIN_FILENO;
+		else
+			file = open(*argv, O_RDONLY);
 		if (file < 0) {
-			// todo: use errno to give helpful error messages
-			// todo: print to stderr
+			// TODO: use errno to give helpful error messages
+			// TODO: print to stderr
 			printf("cat: An error occurred while opening %s.\n", *argv);
 		} else {
 			cat(file);
 		}
-		close(file);
+
+		if (file != STDIN_FILENO)
+			close(file);
 	}
 }
