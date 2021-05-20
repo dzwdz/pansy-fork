8a33dabccecc1b5fd2b3ddb95d3336b51321629b
dzwdz <kg67199@gmail.com>
2021-05-20 18:56:41 +0200

    add the multiplication benchmark i used for optimizing

---
+++ src/bin/bench.c
@@ -0,0 +1,15 @@
+#include <bignum.h>
+#include <unistd.h>
+
+#define SIZE 8192*4
+
+int main() {
+    bignum *a = BN_new(SIZE),
+           *b = BN_new(SIZE),
+           *c = BN_new(SIZE);
+
+    getentropy(a->digits, a->length * sizeof(uint64_t));
+    getentropy(b->digits, b->length * sizeof(uint64_t));
+
+    BN_mul(c, a, b);
+}
