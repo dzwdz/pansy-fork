33cac1d17187a1cf9763d5bc43acab6f57e665e7
dzwdz <kg67199@gmail.com>
2021-05-21 18:28:46 +0200

    division tests

--- src/bin/tests/bignum.c
+++ src/bin/tests/bignum.c
@@ -11,6 +11,8 @@ void test_bignum() {
     bignum *asqr = BN_new(8);
     bignum *b = BN_new(8);
     bignum *c = BN_new(8);
+    bignum *d = BN_new(8);
+    bignum *e = BN_new(8);
 
     bignum *expected = BN_new(8);
 
@@ -20,28 +22,37 @@ void test_bignum() {
     {
         BN_sub(c, a, b);
         BN_fromhex(expected, "100edcc0fededcb43260000320fedcc4320edcccea1adf9");
-        assert(BN_compare(c, expected) == 0);
+        assert(BN_compare(c, expected) == 0); // subtraction
     }
 
     {
         BN_mul(c, a, b);
         BN_fromhex(expected,
             "1122246acceef3812008a067868cf7f897bbfa34ca09621f56438211a49116e75274e2dd9ca21167161a18421599c36");
-        assert(BN_compare(c, expected) == 0);
+        assert(BN_compare(c, expected) == 0); // multiplication
+        BN_div(c, b, d, e);
+        assert(BN_compare(d, a) == 0); // division (dividend)
+        BN_zeroout(expected);
+        assert(BN_compare(e, expected) == 0); // division (remainder)
     }
 
     {
-        bignum *of = BN_new(4);
-        BN_fromhex(of,
+        BN_fromhex(d,
                 "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
 
-        BN_add(c, of, of);
+        BN_add(c, d, d);
         BN_fromhex(expected,
                 "1FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFe");
-        assert(BN_compare(c, expected) == 0);
+        assert(BN_compare(c, expected) == 0); // addition w/ overflow
 
-        BN_sub(c, c, of);
-        assert(BN_compare(c, of) == 0);
+        BN_sub(c, c, d);
+        assert(BN_compare(c, d) == 0); // subtraction w/ overflow
+
+        BN_div(c, b, d, e);
+        BN_fromhex(expected, "feffde04404ba2b55");
+        assert(BN_compare(d, expected) == 0); // division (dividend)
+        BN_fromhex(expected, "c89a393e1deb7340b14e1893ca2ec4ad38298bc4ab8ab96");
+        assert(BN_compare(e, expected) == 0); // division (remainder)
     }
 
     {
@@ -56,7 +67,7 @@ void test_bignum() {
                 BN_mul_karatsuba(c, a, b);
                 BN_fromhex(expected,
                     "1122246acceef3812008a067868cf7f897bbfa34ca09621f56438211a49116e75274e2dd9ca21167161a18421599c36");
-                assert(BN_compare(c, expected) == 0);
+                assert(BN_compare(c, expected) == 0); // karatsuba
             }
         }
         a->length = a_og;
@@ -68,12 +79,14 @@ void test_bignum() {
         BN_modexp_timingsafe(c, b, b, asqr);
         BN_fromhex(expected,
             "a76f78a1708773f866f651af27d7339136c44aab9d7dafeb6087a698a188ec35b5dfea06337ee845d05355938e77c9");
-        assert(BN_compare(c, expected) == 0);
+        assert(BN_compare(c, expected) == 0); // modexp
     }
 
     free(a);
     free(asqr);
     free(b);
     free(c);
+    free(d);
+    free(e);
     free(expected);
 }
